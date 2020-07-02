6f41fb0be2c835f856b5dc0c902974d74f438972
Julien Le Cerf <67580615+JulienL24@users.noreply.github.com>
2020-07-02 17:01:23 +0200

    Update Page.js

--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Page.css';
 
+const content = <section className="Section-central">
+    <p>Popular Series</p>
+    <p>Popular Movies</p>
+</section>
+
 function Page() {
     return(
-        <section className="Section-central">
-        <p>Popular Series</p>
-        <p>Popular Movies</p>
-        </section>
+        content
     )
 }
 
